1764b93314f5517bc9bbc726cd985526eb6e0d1c
isabel <isabel@isabelroses.com>
2024-07-16 15:14:18 +0100

    feat(strings): add mkUpper

--- lib/strings.nix
+++ lib/strings.nix
@@ -1,8 +1,15 @@
+{ lib }:
 let
   # a function that checks if a list contains a list of given strings
   containsStrings =
     list: targetStrings: builtins.all (s: builtins.any (x: x == s) list) targetStrings;
+
+  inherit (lib.strings) toUpper;
+  inherit (builtins) substring stringLength;
+
+  # a function that makes the first letter of a string uppercase
+  mkUpper = str: (toUpper (substring 0 1 str)) + (substring 1 (stringLength str) str);
 in
 {
-  inherit containsStrings;
+  inherit mkUpper containsStrings;
 }
