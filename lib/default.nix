86be63b9daa571f47fa2a4a75a08fd7b2228741d
isabel <isabel@isabelroses.com>
2024-07-16 15:46:09 +0100

    fix: everything that was wrong with lib

--- lib/default.nix
+++ lib/default.nix
@@ -1,20 +1,38 @@
 # following https://github.com/NixOS/nixpkgs/blob/77ee426a4da240c1df7e11f48ac6243e0890f03e/lib/default.nix
 # as a rough template we can create our own extensible lib and expose it to the flake
 # we can then use that elsewhere like our hosts
-{lib0}: let
+{ lib0 }:
+let
   sprout = lib0.makeExtensible (
-    self: let
-      callLib = file: import file {lib = self;};
-      filesets = callLib ./filesets.nix;
-      helpers = callLib ./helpers.nix;
-      strings = callLib ./strings.nix;
-      opts = callLib ./opts.nix;
+    self:
+    let
+      # this allows us to call lib with a reliance on itself
+      # it also saves us repeating the same import line over and over
+      callLib = file: import file { lib = self; };
     in
-      filesets // helpers // strings // opts
-  );
+    {
+      sprout = {
+        fileset = callLib ./fileset.nix;
+        helpers = callLib ./helpers.nix;
+        options = callLib ./options.nix;
+        strings = callLib ./strings.nix;
 
-  # we need to extend gardenLib with the nixpkgs lib to get the full set of functions
-  # if we do it the otherway around we will get errors saying mkMerge and so on don't exist
-  finalLib = sprout.extend (_: _: lib0);
+        inherit (self.sprout.fileset) umport importFilesAndDirs;
+        inherit (self.sprout.helpers) giturl;
+        inherit (self.sprout.options)
+          mkOpt
+          mkOpt'
+          mkBoolOpt
+          mkBoolOpt'
+          mkEnableOpt
+          mkOptions
+          enabled
+          disabled
+          use
+          ;
+        inherit (self.sprout.strings) mkUpper containsStrings;
+      };
+    }
+  );
 in
-  finalLib
+sprout.extend (_: _: lib0)
---
+++ lib/options.nix
@@ -0,0 +1,44 @@
+{ lib }:
+let
+  inherit (lib) mkOption types;
+  # mkOpt type default desc
+  mkOpt =
+    type: default: description:
+    mkOption { inherit type default description; };
+
+  # mkOpt' type default
+  mkOpt' = type: default: mkOpt type default null;
+
+  # mkBoolOpt default desc
+  mkBoolOpt = mkOpt types.bool;
+
+  # mkBoolOpt' default
+  mkBoolOpt' = mkOpt' types.bool;
+
+  # mkEnableOpt name
+  mkEnableOpt = name: mkBoolOpt false "enable ${name}";
+
+  mkOptions = desc: o: { enable = mkBoolOpt false desc; } // o;
+
+  enabled = {
+    enable = true;
+  };
+  disabled = {
+    enable = false;
+  };
+
+  use = o: enabled // o;
+in
+{
+  inherit
+    mkOpt
+    mkOpt'
+    mkBoolOpt
+    mkBoolOpt'
+    mkEnableOpt
+    mkOptions
+    enabled
+    disabled
+    use
+    ;
+}
