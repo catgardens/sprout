dcaf9517dd9a306dbcfa7f538fcadaac04c81a3a
comfysage <67917529+comfysage@users.noreply.github.com>
2024-08-05 10:19:49 +0200

    feat(lib/helpers): add `boolToNum` and `containsStrings` fn

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -1,4 +1,18 @@
 _: let
+  # return an int based on boolean value
+  boolToNum = bool:
+    if bool
+    then 1
+    else 0;
+
+  # a function that checks if a list contains a list of given strings
+  containsStrings = {
+    list,
+    targetStrings,
+  }:
+    builtins.all (s: builtins.any (x: x == s) list) targetStrings;
+
+  # create a git url alias
   giturl = {
     domain,
     alias,
@@ -9,5 +23,5 @@ _: let
     "ssh://${user}@${domain}/".pushInsteadOf = "${alias}:";
   };
 in {
-  inherit giturl;
+  inherit boolToNum containsStrings giturl;
 }
